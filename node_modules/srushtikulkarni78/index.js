33434b34d0146a75be724cd3383ecdc5a37c6aa7
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-09-14 11:41:56 +0530

    publishing my resume as a package

---
+++ node_modules/srushtikulkarni78/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+// 👆 This shebang tells Node to execute this script
+    // for colors (optional)
+import chalk from "chalk";
+// Your resume content
+const resume = `
+${chalk.bold.cyan("Srushti Kulkarni")}
+Backend Developer | React Enthusiast | Node.js | MongoDB
+
+${chalk.bold("Experience")}
+- Frontend Developer @FilmyFunds  Built React dynamic components & inetegrated APIS, Payment Gateway as well.
+- Worked on Node.js backend and React frontend
+
+${chalk.bold("Skills")}
+Node.js | React.js | MongoDB | AWS (learning) |Git Hub | Express.Js | PostreSql | ShadCn | Tailwind Css
+
+${chalk.bold("Projects")}
+- ShutterTale: Photo management system
+- MovieHunt: Movie project management
+- Doc Management System
+- Swiggy Clone
+- Meet Up App Clone
+- Dev-Tinder
+
+${chalk.bold("Contact")}
+LinkedIn: https://linkedin.com/in/srushtikulkarni78
+GitHub: https://github.com/Srushtik942
+Twitter: https://x.com/srushtikul
+`;
+
+console.log(resume);
